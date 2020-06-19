da9e7248c1ff63b774ba8b73e6c75923873ceae9
Uwe Jugel <uwe.jugel@lovoo.com>
2020-06-19 18:37:45 +0200

    generalize test and cover scripts

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -1,18 +1,47 @@
 #/usr/bin/env bash
 set -e
 
-cover() { go test ./... -coverpkg "$pkg" -coverprofile=$prefix.out              | tee $prefix.log; }
-label() { grep -o 'coverage: [^ \.]*' $prefix.log | sed -e 's/: /-/g'           | tee $prefix.txt; }
-link()  { (echo -n "Makefile#"; grep -no "^cover:" Makefile | grep -o '[0-9]*') | tee $prefix.src; }
+cover() { go test ./... $GOTEST_ARGS -coverpkg "$pkg" -coverprofile=$prefix.out   | tee $prefix.log; }
+label() { grep -o 'coverage: [^ \.]*' $prefix.log | sed -e 's/: /-/g' | tail -n 1 | tee $prefix.txt; }
+link()  { (echo -n "Makefile#"; grep -no "^cover:" Makefile | grep -o '[0-9]*')   | tee $prefix.src; }
 
-pkg="$*"
-test -n "$pkg" || pkg=./...
-pkg=`echo "$pkg" | sed -e 's#\([^ ]*\)#./\1/...#g' -e 's# #,#g'`
+update_readme() {
+    set -e
+    if test -e README.md; then
+        if test -e "Makefile"
+        then link=$(link)
+        else link=
+        fi
+        label=$(label)
+        echo "setting label=$label and link=$link in README.md"
+        sed -i -e "s/coverage-[0-9]*\(.*\)(.*)$/$label\1($link)/g" README.md
+    fi
+}
+
+coverpkg=""  # subpackages to be covered
+packages=""  # stand-alone packages to be covered separately
+script=`readlink -f "$0"`
 prefix=.cache/cover
 mkdir -p .cache
 
-echo "covering $pkg"
-cover
-label=$(label)
-link=$(link)
-sed -i -e "s/coverage-[0-9]*\(.*\)(.*)$/$label\1($link)/g" README.md
+for p in $*; do
+    if test -e "$p/go.mod"
+    then packages="$packages $p"
+    else coverpkg="$coverpkg,./$p/..."
+    fi
+done
+
+for p in $packages; do
+    echo "covering stand-alone package $p"
+    if test -e Makefile
+    then make -C $p cover COVERSH=$script
+    else cd $p && pkg=./... prefix=.cache/"$p-cover" cover
+    fi
+done
+
+if test -n "$coverpkg"; then
+    echo "covering packages $coverpkg"
+    test -z "$GOTEST_ARGS" || echo "using GOTEST_ARGS=$GOTEST_ARGS as additional cover arguments"
+    pkg=$coverpkg cover
+    update_readme
+fi
---
+++ scripts/test.sh
@@ -0,0 +1,27 @@
+#/usr/bin/env bash
+set -e
+
+testpkg() {
+    set -e
+    pkg="$1"
+    echo -n "testing '$pkg', "
+    if test -e "$pkg/Makefile"; then
+        echo "found '$pkg/Makefile'"
+        run make -C "$pkg" test
+    else
+        if test -e "$pkg/go.mod"
+        then tests="./...";       echo "found go.mod for package '$pkg', entering dir '$pkg'"; cd "$pkg"
+        else tests="./$pkg/...";  echo "found subpackage '$pkg', running subpackage tests";
+        fi
+
+        mkdir -p .cache
+        prefix=.cache/"$pkg"
+        test -z "$GOTEST_ARGS" || echo "using GOTEST_ARGS=$GOTEST_ARGS as additional go test arguments"
+        run go test -race $GOTEST_ARGS $tests 2>&1 | tee $prefix.log
+        run go vet $tests
+    fi
+}
+
+run() { echo "$*"; "$@"; }
+
+for pkg in $*; do testpkg "$pkg"; done
