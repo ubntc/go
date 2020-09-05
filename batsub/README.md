098d94abeaa03c6857c1d5d7756d5c9710040d37
Uwe Jugel <uwe.jugel@lovoo.com>
2020-09-05 17:55:41 +0200

    add common tests and corner case tests

--- batsub/README.md
+++ batsub/README.md
@@ -1,5 +1,5 @@
 [![Go Report Card](https://goreportcard.com/badge/github.com/ubntc/go/batsub)](https://goreportcard.com/report/github.com/ubntc/go/batsub)
-[![cover-badge](https://img.shields.io/badge/coverage-89%25-brightgreen.svg?longCache=true&style=flat)](Makefile#9)
+[![cover-badge](https://img.shields.io/badge/coverage-96%25-brightgreen.svg?longCache=true&style=flat)](Makefile#9)
 
 # Batched PubSub Reader
 This package implements batched reading of messages from `pubsub.Subscriptions`.
--- batsub/batsub_test.go
+++ batsub/batsub_test.go
@@ -2,6 +2,7 @@ package batsub
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -11,14 +12,16 @@ import (
 )
 
 type testReceiver struct {
-	messages []*pubsub.Message
-	done     chan struct{}
+	messages  []*pubsub.Message
+	sendDelay time.Duration
+	done      chan struct{}
 }
 
 func (rec *testReceiver) Receive(ctx context.Context, f func(context.Context, *pubsub.Message)) error {
 	go func() {
 		for _, m := range rec.messages {
 			f(ctx, m)
+			time.Sleep(rec.sendDelay)
 		}
 		close(rec.done)
 	}()
@@ -27,39 +30,75 @@ func (rec *testReceiver) Receive(ctx context.Context, f func(context.Context, *p
 }
 
 func TestBatchedSubscription(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	rec := &testReceiver{
-		messages: []*pubsub.Message{
-			{ID: "1", Data: []byte("a")},
-			{ID: "2", Data: []byte("b")},
-			{ID: "3", Data: []byte("c")},
-			{ID: "4", Data: []byte("d")},
-			{ID: "5", Data: []byte("e")},
-		},
-		done: make(chan struct{}),
+	type Spec struct {
+		len        int           // number of test messages
+		cap        int           // batch capacity
+		dur        time.Duration // batch interval
+		sendDelay  time.Duration // delay between test messages
+		expBatches int           // number of resulting batches
+		expErr     error         //
 	}
-	sub := NewBatchedSubscription(rec, 2, time.Millisecond)
-
-	var mu sync.Mutex
-	var result []*pubsub.Message
-	var numBatches = 0
-	receive := func(ctx context.Context, messages []*pubsub.Message) {
-		mu.Lock()
-		defer mu.Unlock()
-		numBatches++
-		result = append(result, messages...)
+	cases := map[string]Spec{
+		// common cases
+		"small len": {5, 2, time.Second, 0, 3, nil},
+		"big len":   {1000, 10, time.Second, 0, 100, nil},
+		"small cap": {100, 1, time.Second, 0, 100, nil},
+		"big cap":   {100, 1000, time.Second, 0, 1, nil},
+		// special cases
+		"timeout":  {10, 100, 10 * time.Microsecond, 100 * time.Microsecond, 10, nil},
+		"zero len": {0, 10, time.Second, 0, 0, nil},
+		"zero cap": {10, 0, time.Second, 0, 10, nil},
+		// NOTE: A zero capacity case is valid since Go's `append` will add missing slice capacity
+		//       and the feeding of the slice will be done using a zero capacity (blocking) channel.
 	}
 
-	go func() {
-		<-rec.done
-		cancel()
-		<-ctx.Done()
-	}()
+	var wg sync.WaitGroup
+	for name, spec := range cases {
+		wg.Add(1)
+		go func(name string, spec Spec) {
+			t.Run(name, func(t *testing.T) {
+				defer wg.Done()
+				ctx, cancel := context.WithCancel(context.Background())
+				defer cancel()
 
-	err := sub.ReceiveBatch(ctx, receive)
-	assert.NoError(t, err)
-	assert.Len(t, result, 5)
-	assert.Equal(t, 3, numBatches)
+				data := make([]*pubsub.Message, 0, spec.len)
+				for i := 0; i < spec.len; i++ {
+					data = append(data, &pubsub.Message{ID: fmt.Sprint(i)})
+				}
+
+				assert.Len(t, data, spec.len)
+
+				rec := &testReceiver{
+					messages:  data,
+					done:      make(chan struct{}),
+					sendDelay: spec.sendDelay,
+				}
+				sub := NewBatchedSubscription(rec, spec.cap, spec.dur)
+
+				var mu sync.Mutex
+				var result []*pubsub.Message
+				var numBatches = 0
+				receive := func(ctx context.Context, messages []*pubsub.Message) {
+					mu.Lock()
+					defer mu.Unlock()
+					numBatches++
+					result = append(result, messages...)
+				}
+
+				go func() {
+					<-rec.done
+					cancel()
+					<-ctx.Done()
+				}()
+
+				err := sub.ReceiveBatch(ctx, receive)
+				assert.NoError(t, err)
+				assert.Len(t, result, spec.len)
+				assert.Equal(t, spec.expBatches, numBatches)
+			})
+		}(name, spec)
+	}
+
+	wg.Wait()
+
 }
