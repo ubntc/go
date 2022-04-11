28183c06a7fcb3fd37b3c646009fd63417eb9b33
Uwe Jugel <532284+ubunatic@users.noreply.github.com>
2022-04-11 18:08:22 +0200

    simplily loop

--- playground/tf/variables.tf
+++ playground/tf/variables.tf
@@ -51,14 +51,15 @@ locals {
   // The derived config values also uses the names from the lookup_* tables
   // and include a full project name in the format <unit-short-name>-<stage-name>.
   projects_flattened = merge([
-    for parent, items in var.projects: { for name in keys(items): "${parent}.${name}" => merge(
-      {
-        parent = local.stages_lookup[parent],
-        name = local.project_lookup[name],
-        project = "${local.project_lookup[name]}-${local.stages_lookup[parent]}",
-      },
-      var.projects[parent][name],
-    )}
+    for parent, items in var.projects: {
+      for name, conf in items: "${parent}.${name}" => merge(
+        conf, {
+          parent  = local.stages_lookup[parent],
+          name    = local.project_lookup[name],
+          project = "${local.project_lookup[name]}-${local.stages_lookup[parent]}",
+        }
+      )
+    }
   ]...)
 
   // all configured stage names as flat list
